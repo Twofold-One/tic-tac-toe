fceefaaa492969d92a72445bc7e62392fb12e32f
Twofold-One <evan_geras@mail.ru>
2021-09-29 00:39:41 +0700

    x and o logic and displaying added, turn logic have to be implemented

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,19 @@
 const newPlayer = (name) => {
     const sayHello = () => console.log(`Hi ${name}!`);
     return {name, sayHello};
-}
+};
 
 //gameBoard module
 const gameBoard = (() => {
-    const gameboard = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    let elements = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
     
+    const clear = () => {
+        for (let i = 0; i < 9; i++) {
+            gameBoard.elements[i] = '';
+        }
+    };
 
-    return {gameboard};
-
+    return {elements, clear};
 })();
 
 //displayController
@@ -20,18 +24,103 @@ const displayController = (() => {
         for (let i = 0; i < 9; i++) {
             const gameboardContainer = document.querySelector('#gameboard');
             const gameboardCell = document.createElement('div');
-            gameboardCell.setAttribute('id', `cell-${i}`);
+            gameboardCell.setAttribute('id', `cell`);
+            gameboardCell.setAttribute('data-cell', `${i}`);
             gameboardCell.classList.add('gameboard-cell');
             gameboardContainer.appendChild(gameboardCell);
         }
-    }
+    };
 
     const displayGameboard = () => {
-       const gamebordCell = document.querySelectorAll('.gameboard-cell');
-    }
+        for (let i = 0; i < 9; i++) {
+            const gameboardCell = document.querySelector(`#cell[data-cell='${i}']`);
+            if (gameBoard.elements[i] === 'x') {
+                gameboardCell.classList.add('x');
+            } else if (gameBoard.elements[i] === 'o') {
+                gameboardCell.classList.add('o');
+            } else {
+                gameboardCell.classList.remove('x') || gameboardCell.classList.remove('o');
+            }
+        }
+    };
+    
+    const displayX = () => {
+        const gameboardCell = document.querySelectorAll('#cell');
+        gameboardCell.forEach((cell) => {
+            cell.addEventListener('click', function(e) {
+                gameBoard.elements[e.target.dataset.cell] = 'x';
+                displayController.displayGameboard();
+            });
+        });   
+    };
 
-    return {createGameboard}
+    const displayO = () => {
+        const gameboardCell = document.querySelectorAll('#cell');
+        gameboardCell.forEach((cell) => {
+            cell.addEventListener('click', function(e) {
+                gameBoard.elements[e.target.dataset.cell] = 'o';
+                displayController.displayGameboard();
+            });
+        });   
+    };
+
+    return {createGameboard, displayGameboard, displayX, displayO}
+})();
+displayController.createGameboard();
+displayController.displayGameboard();
+
+//gameFlow module
+const gameFlow = (() => {
+
+    const restart = () => {
+        const restartBtn = document.querySelector('#restart-button');
+        restartBtn.addEventListener('click', () => {
+            gameBoard.clear();
+            displayController.displayGameboard();
+        });
+    };
+
+    const turn = () => {
+        let xTurn = true;
+        let oTurn = false;
+
+        function displayOnTurn() {
+            if (xTurn) {
+                displayController.displayX();
+            } else {
+                displayController.displayO();
+            }
+        };
+
+        function switchTurn() {
+            if (xTurn) {
+                xTurn = false;
+                oTurn = true;
+            } else {
+                xTurn = true;
+                oTurn = false;
+            }
+        }
+
+        if (xTurn) {
+            displayController.displayX();
+            switchTurn();
+        } else if (oTurn) {
+            displayController.displayO();
+            switchTurn();
+        }
+    }
+    return {restart, turn};
 })();
 
-displayController.createGameboard();
+gameFlow.turn();
+gameFlow.restart();
+
+
+
+
+
+
+
+
 
